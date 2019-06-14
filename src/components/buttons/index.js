c2e306ded09d0a5e864fd57c4a82685fa1b6cd94
Anthony Duncan <ADuncan@texasdisposal.com>
2019-06-13 21:52:33 -0500

    EOD 06/13/2019

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStethoscope, faUserInjured, faVideo, faTooth, faGlasses, faPiggyBank, faUserTag } from '@fortawesome/free-solid-svg-icons'
+import classnames from 'classnames';
 import './style.css'
 
 function Buttons(props) {
-    return (
-      <img
-        onClick={() => props.findFriend(props.id)}
-        className="image col-md"
-        src={props.image}
-        alt={props.id}
-      />
-    );
-  }
+  return (
+    <div className="buttonBox">
+     <div className="row">
+      <div className="row text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4 text-center">
+              <button type="button" id="btn" className="btn btn-primary">
+                <FontAwesomeIcon icon={props.icon} />
+              </button>
+              </div>
+              <div className="col-sm-8 text-left">
+                <p>{props.english}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </div>
+  );
+}
 
 export default Buttons;
--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,13 +3,9 @@ import "./style.css";
 
 function videoBox(props) {
   return (
-
-      <div align="center" id="video-box">
-        video box div
-        {/* <span id="video">
-          <iframe width="640" height="480" src="./assets/images/MRF Anniversary Video Anthony Sample.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </span> */}
-      </div>
+    <div align="center" id="video-box">
+      <iframe width="640" height="480" src={props.video} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 
   )
 }
